4a56a496e8057b543b1714ba90ab3334ff93fc49
Youri-Dera <YDera@ilionx.com>
2024-09-10 00:34:34 +0200

    Created entity models and added lombok annotations

---
+++ src/main/java/com/gremlinengine/generator/rest/model/Address.java
@@ -0,0 +1,23 @@
+package com.gremlinengine.generator.rest.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Getter @Setter Long id;
+    private @Getter @Setter String street;
+    private @Getter @Setter String city;
+    private @Getter @Setter String state;
+    private @Getter @Setter String country;
+    private @Getter @Setter String postalCode;
+
+
+}
---
+++ src/main/java/com/gremlinengine/generator/rest/model/Cv.java
@@ -0,0 +1,45 @@
+package com.gremlinengine.generator.rest.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Blob;
+import java.util.List;
+
+@Entity
+public class Cv {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Getter @Setter Long id;
+    private @Getter @Setter String firstName;
+    private @Getter @Setter String lastName;
+    private @Getter @Setter String phoneNumber;
+    private @Getter @Setter String emailAddress;
+    @ManyToOne
+    private @Getter @Setter Address address;
+    private @Getter @Setter String title;
+    @ElementCollection
+    private @Getter @Setter List<String> links;
+    @Lob
+    private @Getter @Setter Blob picture;
+    private @Getter @Setter String aboutMe;
+    private @Getter @Setter String skills;
+    private @Getter @Setter String education;
+    private @Getter @Setter String employmentHistory;
+    private @Getter @Setter String languages;
+    private @Getter @Setter String certificates;
+    private @Getter @Setter String hobbies;
+    private @Getter @Setter String projects;
+    private @Getter @Setter String publications;
+    private @Getter @Setter String awards;
+    private @Getter @Setter String references;
+
+}
