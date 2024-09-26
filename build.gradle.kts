2cc2aa856742bdd6ed896c1fdd21b255d333ef74
Programuesja <gjiniketrina@gmail.com>
2024-09-26 23:07:00 +0200

    WIP: changes to keycloak configuration, added new security class and JWT Converter

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,14 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
+	// SECURITY
+	implementation ("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-config")
+	implementation("org.springframework.security:spring-security-core")
+	implementation("org.springframework.security:spring-security-oauth2-client")
+	implementation("org.springframework.security:spring-security-oauth2-resource-server:5.7.0")
+	implementation("org.springframework.security:spring-security-oauth2-jose:5.7.0")
+
 	// DATA STORAGE
 	implementation("org.springframework.data:spring-data-jpa")
 	// Temporarily using H2 for testing purposes
