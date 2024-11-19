6018071b97657c799488b2201c80dfebf012ed09
DevYouriD <youri.dera@gmail.com>
2024-11-19 12:36:26 +0100

    Restructured build.gradle.kts to simplify version maintenance

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
+
+// Major versions
+val javaVersion = JavaVersion.VERSION_21
+
+// Quality plugin versions
+val pmdVersion = "7.7.0"
+
+// Dependency versions
+val springBootStarterWebVersion = "3.3.5"
+val springBootStarterJpaVersion = "3.3.5"
+val springdocOpenapiStarterWebmvcUiVersion = "2.6.0"
+val hibernateValidatorVersion = "8.0.1.Final"
+val springBootDevtoolsVersion = "3.3.5"
+val lombokVersion = "1.18.36"
+val h2Version = "2.3.232"
+val plantUmlVersion = "8059"
+val springBootStarterTestVersion = "3.3.5"
+val junitPlatformLauncherVersion = "1.11.3"
+
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("pmd")
 }
@@ -10,7 +29,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(javaVersion.toString())
 	}
 }
 
@@ -26,13 +45,13 @@ repositories {
 
 dependencies {
 	// UTILITY
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-	implementation("org.hibernate.validator:hibernate-validator")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-web:$springBootStarterWebVersion")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootStarterJpaVersion")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenapiStarterWebmvcUiVersion")
+	implementation("org.hibernate.validator:hibernate-validator:$hibernateValidatorVersion")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootDevtoolsVersion")
+	compileOnly("org.projectlombok:lombok:$lombokVersion")
+	annotationProcessor("org.projectlombok:lombok:$lombokVersion")
 
 	// SECURITY
 //	implementation ("org.springframework.boot:spring-boot-starter-security")
@@ -44,14 +63,14 @@ dependencies {
 
 	// DATA STORAGE
 	// Temporarily using H2 for testing purposes
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.h2database:h2:$h2Version")
 
 	// DOCUMENTATION
-	implementation("net.sourceforge.plantuml:plantuml:8059")
+	implementation("net.sourceforge.plantuml:plantuml:$plantUmlVersion")
 
 	// TESTING
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootStarterTestVersion")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:$junitPlatformLauncherVersion")
 }
 
 tasks.withType<Test> {
@@ -59,7 +78,7 @@ tasks.withType<Test> {
 }
 
 pmd {
-	toolVersion = "7.7.0"
+	toolVersion = pmdVersion
 	isConsoleOutput = true
 	ruleSets = listOf(
 		"category/java/bestpractices.xml",
