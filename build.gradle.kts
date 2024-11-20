dfeeb445a08f40a038e571fd2c256181751ac701
DevYouriD <youri.dera@gmail.com>
2024-11-21 00:01:06 +0100

    Implemented continuous integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,31 @@
+
+// Major versions
+val javaVersion = JavaVersion.VERSION_21
+
+// Quality plugin versions
+val pmdVersion = "7.7.0"
+val checkstyleVersion = "10.20.1"
+val jacocoVersion = "0.8.12"
+
+// Dependency versions
+val springBootStarterWebVersion = "3.3.5"
+val springBootStarterJpaVersion = "3.3.5"
+val springdocOpenapiStarterWebmvcUiVersion = "2.6.0"
+val hibernateValidatorVersion = "8.0.1.Final"
+val springBootDevtoolsVersion = "3.3.5"
+val lombokVersion = "1.18.36"
+val h2Version = "2.3.232"
+val plantUmlVersion = "8059"
+val springBootStarterTestVersion = "3.3.5"
+val junitPlatformLauncherVersion = "1.11.3"
+
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("pmd")
+	id("checkstyle")
+	id("jacoco")
 }
 
 group = "com.gremlinengine"
@@ -10,7 +33,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(javaVersion.toString())
 	}
 }
 
@@ -26,13 +49,13 @@ repositories {
 
 dependencies {
 	// UTILITY
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-	implementation("org.hibernate.validator:hibernate-validator")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-web:$springBootStarterWebVersion")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootStarterJpaVersion")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenapiStarterWebmvcUiVersion")
+	implementation("org.hibernate.validator:hibernate-validator:$hibernateValidatorVersion")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootDevtoolsVersion")
+	compileOnly("org.projectlombok:lombok:$lombokVersion")
+	annotationProcessor("org.projectlombok:lombok:$lombokVersion")
 
 	// SECURITY
 //	implementation ("org.springframework.boot:spring-boot-starter-security")
@@ -44,16 +67,78 @@ dependencies {
 
 	// DATA STORAGE
 	// Temporarily using H2 for testing purposes
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.h2database:h2:$h2Version")
 
 	// DOCUMENTATION
-	implementation("net.sourceforge.plantuml:plantuml:8059")
+	implementation("net.sourceforge.plantuml:plantuml:$plantUmlVersion")
 
 	// TESTING
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootStarterTestVersion")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:$junitPlatformLauncherVersion")
+}
+
+// Plugin configuration
+
+pmd {
+	toolVersion = pmdVersion
+	isConsoleOutput = true
+	ruleSets = listOf(
+		"category/java/bestpractices.xml",
+		"category/java/codestyle.xml",
+		"category/java/errorprone.xml",
+		"category/java/design.xml",
+		"category/java/performance.xml",
+		"category/java/documentation.xml"
+	)
+	/* Custom ruleset
+	ruleSetFiles = files("config/pmd/ruleset.xml") // Point to your custom ruleset
+    ruleSets = emptyList() // Clear default rule sets to avoid duplication
+	 */
+}
+
+checkstyle {
+	toolVersion = checkstyleVersion
+}
+
+jacoco {
+	toolVersion = jacocoVersion
+}
+
+// Task configuration
+
+tasks.withType<Pmd> {
+	reports {
+		html.required.set(true)
+		xml.required.set(false)
+	}
+	ignoreFailures = false
+}
+
+tasks.withType<Checkstyle> {
+	configFile = file("config/checkstyle/sun_checks.xml")
+	reports {
+		html.required.set(true)
+		xml.required.set(false)
+	}
+	ignoreFailures = false
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.withType<Test>())
+	reports {
+		html.required.set(true)
+		xml.required.set(false)
+	}
+
+	doLast {
+		// Print report browser link to console
+		val reportFile = file("${layout.buildDirectory.get().asFile}/reports/jacoco/test/html/index.html")
+		val fileUrl = reportFile.toURI().toString().removePrefix("file:")
+		println("View report at: file:///$fileUrl")
+	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
 }
